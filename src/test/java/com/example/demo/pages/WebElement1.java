0db589584679ec49e66133b730f7288892eaa42b
Akthab <mhmdakthab6@gmail.com>
2024-05-14 17:10:24 +0530

    feat(): worked with multiple iframes

--- src/test/java/com/example/demo/pages/WebElement1.java
+++ src/test/java/com/example/demo/pages/WebElement1.java
@@ -2,6 +2,7 @@ package com.example.demo.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class WebElement1 {
 
@@ -10,8 +11,23 @@ public class WebElement1 {
 
             driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_iframe");
 
-            driver.findElement(By.linkText("LEARN HTML")).click();
+            //switch to the parent iframe
+            driver.switchTo().frame("iframeResult");
+
+            // find child frame
+            WebElement childframe = driver.findElement(By.tagName("iframe"));
  
+            //switch to the child frame
+            driver.switchTo().frame(childframe);
+
+            // click on html link
+            driver.findElement(By.linkText("Learn HTML")).click();
+
+            // switch to the parent frame
+            driver.switchTo().parentFrame();
+
+            // switch to default content
+            driver.switchTo().defaultContent();
       }
       
 }
